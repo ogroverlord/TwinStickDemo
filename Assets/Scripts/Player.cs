8c8a0d6f345e244f4d47616e444a85c0b54411b2
ogroverlord <karol_michalczyk@interia.pl>
2017-12-13 21:13:28 +0100

    Assets added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput; 
+
+public class Player : MonoBehaviour {
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+    }
+}
